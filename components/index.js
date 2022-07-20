532910130d360ffff751a02e95005233a0874b72
Sammy_Ester <sester2@outlook.com>
2022-07-20 15:37:50 -0500

    The index change

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as Title } from "./Title";
+export { default as Feature } from "./Feature";
+export { default as Preview } from "./Preview";
+export { default as Search } from "./Search";
+export { default as Loading } from "./Loading";
